33be1db09df9387a321b97096ae824bab2a42187
HorvathMatyas <horvath.matyas@stud.uni-obuda.hu>
2021-10-16 21:05:58 +0200

    test Context in Program.cs with using layer Data

--- GPA48P_HFT_2021221.Client/Program.cs
+++ GPA48P_HFT_2021221.Client/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Linq;
+using GPA48P_HFT_2021221.Models;
+using GPA48P_HFT_2021221.Client;
+using GPA48P_HFT_2021221.Data;
 
 namespace GPA48P_HFT_2021221.Client
 {
@@ -6,7 +10,7 @@ namespace GPA48P_HFT_2021221.Client
     {
         static void Main(string[] args)
         {
-
+            AnimalShelterDbContext db = new AnimalShelterDbContext();
         }
     }
 }
