b76e842af7abc2c2a10fc900bb1bae1f26bb6b26
HorvathMatyas <horvath.matyas@stud.uni-obuda.hu>
2021-11-13 11:52:16 +0100

    IPetRepository interface created

---
+++ GPA48P_HFT_2021221.Repository/IPetRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GPA48P_HFT_2021221.Models;
+
+namespace GPA48P_HFT_2021221.Repository
+{
+    public interface IPetRepository
+    {
+        void Create(Pet pet);
+
+        void Delete(int petId);
+
+        Pet Read(int petId);
+
+        IQueryable<Pet> ReadAll();
+
+        void Update(Pet pet);
+    }
+}
