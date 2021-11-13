ec47f2c3b86d63eb9f35b341ad806948f006abe6
HorvathMatyas <horvath.matyas@stud.uni-obuda.hu>
2021-11-13 11:25:19 +0100

    IAnimalShelterRepository interface created

---
+++ GPA48P_HFT_2021221.Repository/IAnimalShelterRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GPA48P_HFT_2021221.Models;
+
+namespace GPA48P_HFT_2021221.Repository
+{
+    public interface IAnimalShelterRepository
+    {
+        void Create(AnimalShelter animalShelter);
+
+        void Delete(int shelterId);
+
+        AnimalShelter Read(int shelterId);
+
+        IQueryable<AnimalShelter> ReadAll();
+
+        void Update(AnimalShelter animalShelter);
+    }
+}
