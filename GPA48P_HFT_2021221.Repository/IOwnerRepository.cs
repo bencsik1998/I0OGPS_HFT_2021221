d04aa8b0b89bf0e3f4276ae6b8ce2edcf5491b0f
HorvathMatyas <horvath.matyas@stud.uni-obuda.hu>
2021-11-13 11:39:53 +0100

    IOwnerRepository interface created

---
+++ GPA48P_HFT_2021221.Repository/IOwnerRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GPA48P_HFT_2021221.Models;
+
+namespace GPA48P_HFT_2021221.Repository
+{
+    public interface IOwnerRepository
+    {
+        void Create(Owner owner);
+
+        void Delete(int ownerId);
+
+        Owner Read(int ownerId);
+
+        IQueryable<Owner> ReadAll();
+
+        void Update(Owner owner);
+    }
+}
