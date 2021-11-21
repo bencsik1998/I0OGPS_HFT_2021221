3f5ae474a65221d6327a8fc559b691bf78551177
HorvathMatyas <horvath.matyas@stud.uni-obuda.hu>
2021-11-21 12:28:13 +0100

    IAnimalShelterLogic interface created

---
+++ GPA48P_HFT_2021221.Logic/IAnimalShelterLogic.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GPA48P_HFT_2021221.Models;
+
+namespace GPA48P_HFT_2021221.Logic
+{
+    public interface IAnimalShelterLogic
+    {
+        void Create(AnimalShelter animalShelter);
+
+        void Delete(int shelterId);
+
+        AnimalShelter Read(int shelterId);
+
+        IEnumerable<AnimalShelter> ReadAll();
+
+        void Update(AnimalShelter animalShelter);
+    }
+}
