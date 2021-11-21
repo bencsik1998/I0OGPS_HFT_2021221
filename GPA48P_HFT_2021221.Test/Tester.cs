8b8ad67da380e3bd0f2be8b5fec6a3b7732090d9
HorvathMatyas <horvath.matyas@stud.uni-obuda.hu>
2021-11-21 17:47:40 +0100

    Tester class created

---
+++ GPA48P_HFT_2021221.Test/Tester.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GPA48P_HFT_2021221.Logic;
+using GPA48P_HFT_2021221.Models;
+using GPA48P_HFT_2021221.Repository;
+using NUnit.Framework;
+using Moq;
+
+namespace GPA48P_HFT_2021221.Test
+{
+    [TestFixture]
+    public class Tester
+    {
+
+    }
+}
