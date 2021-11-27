a46a34870475eaba5a5f09cd43d3e17183b25907
HorvathMatyas <horvath.matyas@stud.uni-obuda.hu>
2021-11-27 12:43:25 +0100

    added Transients, edited localhost port, modified AnimalShelterController, added JsonIgnore to AnimalShelter.cs

---
+++ GPA48P_HFT_2021221.Endpoint/Controllers/AnimalShelterController.cs
@@ -0,0 +1,56 @@
+﻿using GPA48P_HFT_2021221.Models;
+using GPA48P_HFT_2021221.Repository;
+using GPA48P_HFT_2021221.Logic;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace GPA48P_HFT_2021221.Endpoint.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AnimalShelterController : ControllerBase
+    {
+        IAnimalShelterLogic asl;
+
+        public AnimalShelterController(IAnimalShelterLogic asl)
+        {
+            this.asl = asl;
+        }
+
+        // GET: /animalShelter
+        [HttpGet]
+        public IEnumerable<AnimalShelter> Get()
+        {
+            return asl.ReadAll();
+        }
+
+        // GET api/<AnimalShelterController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<AnimalShelterController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<AnimalShelterController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<AnimalShelterController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- GPA48P_HFT_2021221.Endpoint/Startup.cs
+++ GPA48P_HFT_2021221.Endpoint/Startup.cs
@@ -1,3 +1,7 @@
+using GPA48P_HFT_2021221.Models;
+using GPA48P_HFT_2021221.Data;
+using GPA48P_HFT_2021221.Repository;
+using GPA48P_HFT_2021221.Logic;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -15,6 +19,16 @@ namespace GPA48P_HFT_2021221.Endpoint
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddTransient<IAnimalShelterLogic, AnimalShelterLogic>();
+            services.AddTransient<IOwnerLogic, OwnerLogic>();
+            services.AddTransient<IPetLogic, PetLogic>();
+
+            services.AddTransient<IAnimalShelterRepository, AnimalShelterRepository>();
+            services.AddTransient<IOwnerRepository, OwnerRepository>();
+            services.AddTransient<IPetRepository, PetRepository>();
+
+            services.AddTransient<AnimalShelterDbContext, AnimalShelterDbContext>();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
