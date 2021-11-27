0d8597f7a23ea494dcb08bb9e89a24f059bf6e7d
HorvathMatyas <horvath.matyas@stud.uni-obuda.hu>
2021-11-27 13:45:52 +0100

    PetController.cs is done and deleted an empty line in AnimalShelterController.cs

--- GPA48P_HFT_2021221.Endpoint/Controllers/AnimalShelterController.cs
+++ GPA48P_HFT_2021221.Endpoint/Controllers/AnimalShelterController.cs
@@ -7,7 +7,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-
 namespace GPA48P_HFT_2021221.Endpoint.Controllers
 {
     [Route("[controller]")]
---
+++ GPA48P_HFT_2021221.Endpoint/Controllers/PetController.cs
@@ -0,0 +1,58 @@
+﻿using GPA48P_HFT_2021221.Models;
+using GPA48P_HFT_2021221.Repository;
+using GPA48P_HFT_2021221.Logic;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GPA48P_HFT_2021221.Endpoint.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class PetController : ControllerBase
+    {
+        IPetLogic pl;
+
+        public PetController(IPetLogic pl)
+        {
+            this.pl = pl;
+        }
+
+        // GET: /pet
+        [HttpGet]
+        public IEnumerable<Pet> Get()
+        {
+            return pl.ReadAll();
+        }
+
+        // GET /pet/id
+        [HttpGet("{id}")]
+        public Pet Get(int id)
+        {
+            return pl.Read(id);
+        }
+
+        // POST /pet
+        [HttpPost]
+        public void Post([FromBody] Pet value)
+        {
+            pl.Create(value);
+        }
+
+        // PUT /pet
+        [HttpPut("{id}")]
+        public void Put([FromBody] Pet value)
+        {
+            pl.Update(value);
+        }
+
+        // DELETE /pet/id
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            pl.Delete(id);
+        }
+    }
+}
