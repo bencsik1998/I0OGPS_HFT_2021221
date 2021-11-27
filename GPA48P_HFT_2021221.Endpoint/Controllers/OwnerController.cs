ac5ec897dc6704fbe7928b67000d530cb88b5302
HorvathMatyas <horvath.matyas@stud.uni-obuda.hu>
2021-11-27 13:24:58 +0100

    OwnerController.cs is done

---
+++ GPA48P_HFT_2021221.Endpoint/Controllers/OwnerController.cs
@@ -0,0 +1,58 @@
+﻿using GPA48P_HFT_2021221.Models;
+using GPA48P_HFT_2021221.Repository;
+using GPA48P_HFT_2021221.Logic;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GPA48P_HFT_2021221.Endpoint.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class OwnerController : ControllerBase
+    {
+        IOwnerLogic ol;
+
+        public OwnerController(IOwnerLogic ol)
+        {
+            this.ol = ol;
+        }
+
+        // GET: /owner
+        [HttpGet]
+        public IEnumerable<Owner> Get()
+        {
+            return ol.ReadAll();
+        }
+
+        // GET /owner/id
+        [HttpGet("{id}")]
+        public Owner Get(int id)
+        {
+            return ol.Read(id);
+        }
+
+        // POST /owner
+        [HttpPost]
+        public void Post([FromBody] Owner value)
+        {
+            ol.Create(value);
+        }
+
+        // PUT /owner
+        [HttpPut("{id}")]
+        public void Put([FromBody] Owner value)
+        {
+            ol.Update(value);
+        }
+
+        // DELETE /owner/id
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            ol.Delete(id);
+        }
+    }
+}
