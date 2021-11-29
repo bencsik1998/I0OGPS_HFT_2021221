f3aa125b5bd52dd015f9efbe848a7c5360d6938b
HorvathMatyas <horvath.matyas@stud.uni-obuda.hu>
2021-11-29 21:29:26 +0100

    added restervices to Client -> Main

--- GPA48P_HFT_2021221.Client/Program.cs
+++ GPA48P_HFT_2021221.Client/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace GPA48P_HFT_2021221.Client
+﻿using GPA48P_HFT_2021221.Models;
+using System;
+
+namespace GPA48P_HFT_2021221.Client
 {
     class Program
     {
         static void Main(string[] args)
         {
+            System.Threading.Thread.Sleep(8000);
 
+            RestService rest = new RestService("http://localhost:62480");
+
+            rest.Post<Pet>(new Pet()
+            {
+                Class = "Madár"
+            }, "pet");
+
+            rest.Post<Owner>(new Owner()
+            {
+                FirstName = "Jancsika"
+            }, "owner");
+
+            var pets = rest.Get<Pet>("pet");
+            var animalShelters = rest.Get<AnimalShelter>("animalShelter");
+            var owners = rest.Get<Owner>("owner");
+
+            var AvarageAgeOfPets = rest.GetSingle<double>("/stat/avarageageofpets");
+
+            ;
         }
     }
 }
